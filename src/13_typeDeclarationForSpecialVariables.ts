82c7f046af9ff0a4dac5295ba7c8be5da7b3ddaa
BugBuster <devrath.rana@bold.com>
2025-07-30 21:22:43 +0530

    type declaration done

---
+++ src/13_typeDeclarationForSpecialVariables.ts
@@ -0,0 +1,5 @@
+declare const process: any; // This is a special variable that is available in Node.js environment, it contains information about the current Node.js process.
+
+// declare const process: any="hello"; //Initializers are not allowed in ambient contexts. as we shouldn't assign a value to a variable that is declared using declare keyword for type declaration purposes only
+
+const USER = process.env.USER || "defaultUser"; // This is a special variable that holds the username of the current user, or a default value if not available.
